7d154b2ea8b8b1db1875875ef1d53c9fa7b46426
Leona <iosef1004@hotmail.com>
2021-12-14 13:23:27 -0500

    putting this game in my repos

---
+++ Assets/Scripts/Scorer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    private int score = 0;
+    private void OnCollisionEnter(Collision other) 
+    {
+        if(other.gameObject.tag != "Hit") {
+            score++;
+            Debug.Log("You bumped into things this many times: " + score);
+        }
+    }
+}
